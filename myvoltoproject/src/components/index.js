13b4c200bc701de0db9c15cb0439ca27ab3dc484
LorenzoCervi <48015106+LorenzoCervi@users.noreply.github.com>
2020-11-26 09:35:53 +0100

    Add files via upload

---
+++ myvoltoproject/src/components/index.js
@@ -0,0 +1,19 @@
+/**
+ * Add your components here.
+ * @module components
+ * @example
+ * import Footer from './Footer/Footer';
+ *
+ * export {
+ *   Footer,
+ * };
+ */
+import EmailWidget from './EmailWidget/EmailWidget';
+import NumberWidget from './NumberWidget/NumberWidget';
+
+import SelectMyWidget from './SelectMyWidget/SelectMyWidget';
+
+import MyFormView from './Blocks/MyComp/View';
+import MyFormEdit from './Blocks/MyComp/Edit';
+
+export { MyFormView, MyFormEdit, EmailWidget, NumberWidget, SelectMyWidget };
